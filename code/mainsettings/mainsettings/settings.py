92c915f2bb4efe79eb95d8f5b7a71871dc8a8364
arturandre <arturpossible@gmail.com>
2022-03-13 22:30:35 -0500

    mapretriever: demo0 buffer + shp.js and proj4.js

--- code/mainsettings/mainsettings/settings.py
+++ code/mainsettings/mainsettings/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',    
-    'userwebsite',    
+    'mapretriever',
+    'userwebsite',
 ]
 
 MIDDLEWARE = [
@@ -122,8 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
