6c98c59df4eeb336a5639aa4dc7c927cd4bdc0f8
Artur Andre <arturpossible@gmail.com>
2022-08-23 16:31:02 -0500

    ol updated and made available locally

--- code/mainsettings/userwebsite/urls.py
+++ code/mainsettings/userwebsite/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.Home, name='Home'),
+    path('',views.Home2, name='Home2'),
     path('home',views.Home2, name='Home2'),
     path('get_report',views.get_report, name='get_report'),
     path('filterbybbox',views.filter_by_bbox, name='filterbybbox'),
